516a3163c6db83dd11777c9604e27dbac101fe47
spetseffect <spetseffect@gmail.com>
2021-03-06 23:42:09 +0200

    T Max(T ... args)

---
+++ src/com/company/Methods.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Methods<T extends Number> {
+    public T Max(T ... args){
+        T result = args[0];
+        for (int i=1;i<args.length;i++) {
+            if ((Double)result < (Double)args[i]) result = args[i];
+        }
+        return result;
+    }
+}

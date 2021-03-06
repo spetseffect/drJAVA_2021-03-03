603a936347cbca79f6f730e58755146ea34547c9
spetseffect <spetseffect@gmail.com>
2021-03-06 23:44:01 +0200

    T Min(T ... args)

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -8,4 +8,11 @@ public class Methods<T extends Number> {
         }
         return result;
     }
+    public T Min(T ... args){
+        T result = args[0];
+        for (int i=1;i<args.length;i++) {
+            if ((Double)result > (Double)args[i]) result = args[i];
+        }
+        return result;
+    }
 }

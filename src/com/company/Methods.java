566436b358018792945b6df7f928cfe94c3a7e50
spetseffect <spetseffect@gmail.com>
2021-03-07 01:14:50 +0200

    sort array

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Methods<T extends Number> {
+    //search max value
     public T Max(T ... args){
         T result = args[0];
         for (int i = 1; i < args.length; i++) {
@@ -8,6 +9,7 @@ public class Methods<T extends Number> {
         }
         return result;
     }
+    //search min value
     public T Min(T... args){
         T result = args[0];
         for (int i = 1; i < args.length; i++) {
@@ -15,6 +17,7 @@ public class Methods<T extends Number> {
         }
         return result;
     }
+    //get average
     public double Avg(T ... args){
         if(args.length == 0) return 0;
         double sum = 0;
@@ -22,5 +25,20 @@ public class Methods<T extends Number> {
             sum += (double)args[i];
         }
         return sum/args.length;
+    }
+    //sort array
+    public void InsertSort(T[] array, boolean increase) {
+        int size = array.length;
+        for (int i = 1; i < size; i++) {
+            Double tmp = (Double) array[i];
+            int j = i - 1;
+            for (; j >= 0 && (increase ? ((Double)array[j] > tmp) : ((Double)array[j] < tmp)); j--) {
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = (T) tmp;
+        }
+    }
+    public void InsertSort(T[] array) {
+        InsertSort(array, true);
     }
 }

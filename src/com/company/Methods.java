32ba338d2112a72ffe72e2e0a3f6588f90c5c187
spetseffect <spetseffect@gmail.com>
2021-03-07 00:14:43 +0200

    double Avg(T ... args)

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -3,16 +3,24 @@ package com.company;
 public class Methods<T extends Number> {
     public T Max(T ... args){
         T result = args[0];
-        for (int i=1;i<args.length;i++) {
+        for (int i = 1; i < args.length; i++) {
             if ((Double)result < (Double)args[i]) result = args[i];
         }
         return result;
     }
-    public T Min(T ... args){
+    public T Min(T... args){
         T result = args[0];
-        for (int i=1;i<args.length;i++) {
+        for (int i = 1; i < args.length; i++) {
             if ((Double)result > (Double)args[i]) result = args[i];
         }
         return result;
+    }
+    public double Avg(T ... args){
+        if(args.length == 0) return 0;
+        double sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            sum += (double)args[i];
+        }
+        return sum/args.length;
     }
 }

34d2649d0ff78614f15c608259b68c2efedc9124
spetseffect <spetseffect@gmail.com>
2021-03-07 03:03:46 +0200

    int BinSearch(T[] array, T value)

--- src/com/company/Methods.java
+++ src/com/company/Methods.java
@@ -18,13 +18,15 @@ public class Methods<T extends Number> {
         return result;
     }
     //get average
-    public static <T> double Avg(T ... args){
-        if(args.length == 0) return 0;
-        double sum = 0;
-        for (int i = 0; i < args.length; i++) {
-            sum += (double)args[i];
+    public static <T> double Avg(T ... args) throws Exception{
+        if(args.length == 0) throw new Exception("Array is empty.");
+        else {
+            double sum = 0;
+            for (int i = 0; i < args.length; i++) {
+                sum += (double) args[i];
+            }
+            return sum / args.length;
         }
-        return sum/args.length;
     }
     //sort array
     public static <T> void InsertSort(T[] array, boolean increase) {
@@ -40,5 +42,20 @@ public class Methods<T extends Number> {
     }
     public static <T> void InsertSort(T[] array) {
         InsertSort(array, true);
+    }
+    //binary search
+    public static <T> int BinSearch(T[] array, T value) {
+        InsertSort(array);//if array not sorted
+        int ind=array.length / 2;
+        Double tmp = (Double) array[ind];
+        Double s = (Double) value;
+        int b = 0, e = array.length - 1;
+        while(!tmp.equals(s) && b < e){
+            if(tmp < s) e = ind-1;
+            else b = ind + 1;
+            ind = (b + e) / 2;
+            tmp = (Double) array[ind];
+        }
+        return ind;
     }
 }

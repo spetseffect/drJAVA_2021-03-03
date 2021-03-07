dd8c7b862e37858fface169031facc6d3c461f78
spetseffect <spetseffect@gmail.com>
2021-03-07 05:49:09 +0200

    testing Methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Integer[] arr=new Integer[]{78,4751,683,647,58,0};
+        System.out.println(Methods.BinSearch(arr,1));
+        //Methods.InsertSort(arr);
+        Methods.OutputToConsole(arr,", ");
     }
 }
